decccadfa81e7f6955816824cbc30151e10623e9
pcrainer38 <pammie_78704@yahoo.com>
2023-10-02 21:20:16 -0500

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // DOM elements
 var questionEl = document.querySelector("#question");
-// var optionsEl = document.querySelector("#options");
+var optionsEl = document.querySelector("#options");
 // var submitBtn = document.querySelector("#submit");
 // var startBtn = document.querySelector("#start");
 // var initialsEl = document.querySelector("#initials");
-// var feedback = document.querySelector("#feedback");
+var feedback = document.querySelector("#feedback");
 
 var currentQuestionIndex = 0;
 
@@ -19,7 +19,7 @@ function playGame() {
     timer();
     getQuestion();    
 }
-
+// timer
 function timer() {
     var sec = question.length * 5;
     var timer = setInterval(function() {
@@ -31,16 +31,54 @@ function timer() {
     }, 1000);
 }
 
-
 function getQuestion() {
-    var questionContainer = document.getElementById("question");
+    var currentQuestion = question[currentQuestionIndex];
 
-    question.forEach(function(questionObj) {
-        var questionElement = document.createElement("div");
-        questionElement.innerText = questionObj.question;
+    var titleEl = document.getElementById("questiontitle");
+    titleEl.textContent = currentQuestion.title;
 
-        questionContainer.appendChild(questionElement);
-    }) 
+    optionsEl.innerHTML = "";
 
+    currentQuestion.options.forEach(function(choice, i) {
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "options");
+        choiceNode.setAttribute("value", options);
+
+        choiceNode.textContent = i + 1 + ". " + options;
+
+        choiceNode.onclick = questionClick;
+
+        optionsEl.appendChild(choiceNode);
+    });
+}
+
+function questionClick() {
+    if (this.value !== question[currentQuestionIndex].answer) {
+        time -= 10;
+
+        if (time < 0) {
+            time = 0;
+        }
+
+        timerEl.textContent = time;
+        feedbackEl.textContent = "Wrong!";
+        feedbackEl.style.color = "red";
+        feedbackEl.style.fontSize = "400%";
+    } else {
+        feedbackEl.textContent = "Correct!";
+        feedbackEl.style.color = "green";
+        feedbackEl.style.fontSize = "400%";
+    }
+
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex === question.length) {
+        quizEnd();
+    }
 }
 
